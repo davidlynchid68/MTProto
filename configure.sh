378fff6cb34c291bdc37136e85ca42c269f62718
a132395 <67187730+a132395@users.noreply.github.com>
2021-02-08 21:10:03 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,24 +2,23 @@
 
 # Download and install V2Ray
 mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -rf /tmp/xray
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
   "log": {
     "loglevel": "none"
   },
   "inbounds": [
    {
-  "port": 443,
+  "port": $PORT,
   "protocol": "mtproto",
   "settings": {
     "users": [{"secret": "$SECRET"}]
@@ -35,4 +34,4 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 EOF
 
 # Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
